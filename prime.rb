399a4164ec5a2464b80387520efaa5a142623a83
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-09 02:40:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,18 +2,9 @@
 #takes in an integer argument
 #returns a boolean of whether aor not that intiger is a prime number
 
-def prime?(x)
-  if x == 1
-    return true
-  elsif x < 1
-    return false
-  else
-  for y in 2..(x-1)
-    if (x % y) == 0
-      return false
-    end
+def prime?(num)
+  (2..(num - 1)).each do |n|
+    return false if num % n == 0
   end
-end
-
   true
 end
