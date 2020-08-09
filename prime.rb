c37491cef80b62112134348bac32a9dbf32681ad
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-09 03:07:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,9 @@
 #returns a boolean of whether aor not that intiger is a prime number
 
 def prime?(num)
+  if num < 2
   (2..(num - 1)).each do |n|
-    return false if num < 2
+    return false if num % n == 0
   end
   true
 end
