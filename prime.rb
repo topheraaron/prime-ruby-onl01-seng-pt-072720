4e6f1433cad282bbc262b2ac8a27df20940cfe34
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-09 03:11:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+# defining a prime method prime?()
+#takes in an integer argument
+#returns a boolean of whether aor not that intiger is a prime number
+
+def prime?(num)
+  if num < 2
+    return false
+  end
+  (2..(num - 1)).each do |n|
+    return false if num % n == 0
+  end
+  true
+end
